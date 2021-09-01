1219fb45cf75b2129ec988a6d1f82858f58f76a7
bruce <bancheta@student.ubc.ca>
2021-09-01 06:17:12 -0700

    refactoring foo into testArray

---
+++ src/testcode/main.java
@@ -0,0 +1,35 @@
+package testcode;
+
+class main {
+    public static void main(String[] args) {
+        int[] testArray = {0, 1, 2, 3, 4};
+
+        int[] bar = new int[100];
+        for(int i = 0; i < 100; i++){
+            bar[i] = i;
+        }
+        System.out.println("The number of even numbers in testArray is:");
+        System.out.println(countEvenNum(testArray, 5));
+
+        System.out.println("The number of even numbers in bar is:");
+        System.out.println(countEvenNum(bar, 100));
+
+    }
+
+
+
+    private static int countEvenNum(int[] arr, int size){
+        int count = 0;
+
+        for(int i = 0; i < size; i++){
+            if(arr[i] % 2 == 0){
+                count++;
+            }
+            else{
+
+            }
+        }
+
+        return count;
+    }
+}
